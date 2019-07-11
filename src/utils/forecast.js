743e8c9f554f8a9174725b1e5e86b39ba8238058
Meng Khunnit <mengkhunnit@Mengs-MacBook-Pro.local>
2019-07-11 18:33:38 +0700

    Commit all files

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/d02c9236189daa281fa26a07a4254e2d/'+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude) +''
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
